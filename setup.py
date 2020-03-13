4ff3efd6a84860115dfcbfd9e104ea663f7b977a
retxxxirt <retxxxirt@gmail.com>
2020-03-13 19:35:10 +0800

    Added travis ci auto-deployment to pypi.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='slata-parser',
+    packages=['slata_parser'],
+    version='0.1.0',
+    license='MIT',
+    description='Slata shop parser.',
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    author='retxxxirt',
+    author_email='retxxirt@gmail.com',
+    url='https://github.com/retxxxirt/slata-parser',
+    keywords=['slata', 'slata parser', 'slata api', 'slata crawler']
+)
